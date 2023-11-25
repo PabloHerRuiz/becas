738e0173828dd19397a55d8199587cc0933b3086
PabloHerRuiz <pherrui680@g.educaand.es>
2023-11-25 17:28:38 +0100

    intento de pagina unica

---
+++ vistas/enruta.php
@@ -0,0 +1,15 @@
+<?php
+if (!isset($_GET['menu'])) {
+    $_GET['menu'] = "login";
+}
+if (isset($_GET['menu'])) {
+    if ($_GET['menu'] == "inicio") {
+        require_once $_SERVER["DOCUMENT_ROOT"] . '/index.php';
+
+    }
+    if ($_GET['menu'] == "login") {
+        require_once $_SERVER["DOCUMENT_ROOT"] . '/vistas/identificacion.php';
+
+    }
+}
+?>
