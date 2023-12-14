b9fbbb3ddfb91cdfd22b76bfd9e732892b3a7e2e
PabloHerRuiz <pherrui680@g.educaand.es>
2023-12-14 17:24:38 +0100

    cambios perfil

--- vistas/perfil.php
+++ vistas/perfil.php
@@ -12,25 +12,23 @@
     <div id="contenedor">
         <h2>Perfil de Candidato</h2>
         <form id="formPerfil">
-            <label for="dni">DNI:</label><br>
+            <label for="dni">DNI:</label>
             <p id="dni" name="dni">aqui vendria el dni del candidato</p>
-            <label for="nombre">Nombre:</label><br>
+            <label for="nombre">Nombre:</label>
             <input type="text" id="nombre" name="nombre" data-valida="relleno"><br>
-            <label for="apellidos">Apellidos:</label><br>
+            <label for="apellidos">Apellidos:</label>
             <input type="text" id="apellidos" name="apellidos" data-valida="relleno"><br>
-            <!-- <label for="password">Contraseña:</label><br>
-        <input type="password" id="password" name="password"><br> -->
-            <label for="curso">Curso:</label><br>
+            <label for="curso">Curso:</label>
             <select id="curso" name="curso" data-valida="relleno">
                 <option selected disabled>Selecciona un curso</option>
             </select><br>
-            <label for="correo">Correo:</label><br>
+            <label for="correo">Correo:</label>
             <input type="email" id="correo" name="correo" data-valida="email"><br>
-            <label for="telefono">Teléfono:</label><br>
+            <label for="telefono">Teléfono:</label>
             <input type="tel" id="telefono" name="telefono" data-valida="telefono"><br>
-            <label for="domicilio">Domicilio:</label><br>
+            <label for="domicilio">Domicilio:</label>
             <input type="text" id="domicilio" name="domicilio" data-valida="relleno"><br>
-            <label for="fecha_nacimiento">Fecha de Nacimiento:</label><br>
+            <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
             <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" data-valida="fecha"><br>
             <input type="submit" value="Guardar Cambios">
         </form>
